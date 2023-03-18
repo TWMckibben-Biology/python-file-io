2a4cadb99c6303df1d93878605fa07526886da35
TaylorMckibben <twm0009@auburn.edu>
2023-03-18 16:07:42 -0500

    Man I am killing it. Actually pushing the versions of my script as I add code. Just playing around with open and with function

--- text_parser.py
+++ text_parser.py
@@ -1,10 +1,18 @@
 #! /usr/bin/env python3
 print('Hello')
 
-import re
 import sys
+import re
 
+#wordsearch = "*herit*"
+#wordcompiler = re.compile(wordsearch)
 
+f = open('origin.txt')
+#hits = wordcompiler('origin.txt')
+#print(hits)
+print(f.name)
+
+f.close()
 
 
 if__name__ = '__main__'
