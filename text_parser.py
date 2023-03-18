f50b6be364499a0e5f0b3a229b5a675f8e8ed442
TaylorMckibben <twm0009@auburn.edu>
2023-03-18 16:54:54 -0500

    Lol, that did not work. I got the print statements to work, but not the write. Python does not like printing variables and using regex.

--- text_parser.py
+++ text_parser.py
@@ -4,6 +4,11 @@ print('Hello')
 import sys
 import re
 
+
+####
+#       ="herit" returned just herit
+#       ="*herit" and "herit*" returned good hits but not entire words
+
 wordsearch = "\w*herit\w*"
 wordcompiler = re.compile(wordsearch, re.IGNORECASE)
 
@@ -12,7 +17,11 @@ with open('origin.txt', 'r') as rf:
         for line_index, line in enumerate(rf, 1):
             if wordcompiler.findall(line):
                 hits = wordcompiler.search(line)
-                print(hits)
+                #print(hits)
+                #print(line_index)
+                #print({line_index},{hits})
+                #print(line_index,hits.group())  This is what I actually want, just need to tab separate and newline
+                wf.write('line_index\thits.group\n')
 print('Goodbye')
         
 
